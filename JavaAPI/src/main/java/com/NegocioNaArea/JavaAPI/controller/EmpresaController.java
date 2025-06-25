deefca460742edd2d6ae722dc48b01e4324851aa
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-25 14:46:02 -0300

    feat: criar método delete no controller

--- JavaAPI/src/main/java/com/NegocioNaArea/JavaAPI/controller/EmpresaController.java
+++ JavaAPI/src/main/java/com/NegocioNaArea/JavaAPI/controller/EmpresaController.java
@@ -1,4 +1,22 @@
 package com.NegocioNaArea.JavaAPI.controller;
 
+import com.NegocioNaArea.JavaAPI.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmpresaController {
+    @Autowired
+    private EmpresaService empresaService;
+
+    //métodos
+    @DeleteMapping("/{cnpj}")
+    //responseEntity representa as respostas HTTP
+    public ResponseEntity<Void> deletarPorCnpj(@PathVariable String cnpj) {
+        empresaService.deletarPorCnpj(cnpj);
+        return ResponseEntity.noContent().build(); // retorna 204 No Content
+    }
 }
