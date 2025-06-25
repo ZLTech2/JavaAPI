5454a9ad86fa8b8554af52aa18807a51d0466434
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-25 16:18:59 -0300

    feat: adicionar entidade modelo de produto

---
+++ JavaAPI/src/main/java/com/NegocioNaArea/JavaAPI/model/Produto.java
@@ -0,0 +1,74 @@
+package com.NegocioNaArea.JavaAPI.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeProduto;
+
+    private String descricaoProduto;
+
+    private double preco;
+
+    @Column(nullable = false)
+    private LocalDateTime dataPostagem;
+
+    @Column(nullable = false)
+    private String contatoEmpresa;
+
+    public String getContatoEmpresa() {
+        return contatoEmpresa;
+    }
+
+    public void setContatoEmpresa(String contatoEmpresa) {
+        this.contatoEmpresa = contatoEmpresa;
+    }
+
+    public LocalDateTime getDataPostagem() {
+        return dataPostagem;
+    }
+
+    public void setDataPostagem(LocalDateTime dataPostagem) {
+        this.dataPostagem = dataPostagem;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
